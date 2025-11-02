d3cf433e8118981e628126eedd7866eca44f11d1
Ernst Padron <97245274+APopcorn@users.noreply.github.com>
2025-11-02 11:29:14 +0100

    Add react-router-dom and set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,34 @@ import Projects from "./components/projects/Projects";
 import { useRef } from "react";
 
 interface CustomRef {
-  homeRefPoint: React.RefObject<HTMLInputElement>,
-  aboutMeRefPoint: React.RefObject<HTMLInputElement>,
-  projectsRefPoint: React.RefObject<HTMLInputElement>,
+    homeRefPoint: React.RefObject<HTMLInputElement>;
+    aboutMeRefPoint: React.RefObject<HTMLInputElement>;
+    projectsRefPoint: React.RefObject<HTMLInputElement>;
 }
 
 function App() {
-  const homeRef = useRef<HTMLInputElement>(null);
-  const aboutMeRef = useRef<HTMLInputElement>(null);
-  const projectsRef = useRef<HTMLInputElement>(null);
+    const homeRef = useRef<HTMLInputElement>(null);
+    const aboutMeRef = useRef<HTMLInputElement>(null);
+    const projectsRef = useRef<HTMLInputElement>(null);
 
-  const refsCluster = {
-    homeRefPoint: homeRef,
-    aboutMeRefPoint: aboutMeRef,
-    projectsRefPoint: projectsRef,
-  };
+    const refsCluster = {
+        homeRefPoint: homeRef,
+        aboutMeRefPoint: aboutMeRef,
+        projectsRefPoint: projectsRef,
+    };
 
-  const refs = useRef<CustomRef>(refsCluster);
+    const refs = useRef<CustomRef>(refsCluster);
 
-  return (
-    <>
-      <div className="flex flex-col bg-white">
-        <NavBar ref={refs} />
-        <HomeContainer ref={homeRef} />
-        <AboutMe ref={aboutMeRef} />
-        <Projects ref={projectsRef} />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex flex-col bg-white">
+                <NavBar ref={refs} />
+                <HomeContainer ref={homeRef} />
+                <AboutMe ref={aboutMeRef} />
+                <Projects ref={projectsRef} />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
